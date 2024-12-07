08722fd67db6acb9484223157f08375c45479be3
c0956007 <c0956007@mylambton.ca>
2024-12-07 14:50:02 -0500

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+/*
+  Student Name: Pargat Singh
+  File Name: script.js
+  Date: 28 November, 2024
+*/
+
+// Hamburger menu function
+function hamburger() {
+  var menu = document.getElementById("menu-links");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
